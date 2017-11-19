LineageOS/android_device_lineage_sepolicy

sepolicy: Move Lineage SDK policy to platform
Change-Id: Iae32c09e7c0d1dd07bc8978bd0419eeec7ed876c

common/sepolicy.mk
@@ -3,5 +3,8 @@
 # inherit from Lineage
 #
 
-BOARD_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/common
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
+    device/lineage/sepolicy/common/public
+
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/lineage/sepolicy/common/private

teemodk/device_aicp_sepolicy

common/sepolicy.mk
@@ -3,5 +3,8 @@
 # inherit from AICP
 #
 
-BOARD_SEPOLICY_DIRS += \
-    device/aicp/sepolicy/common
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
+    device/aicp/sepolicy/common/public
+
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/aicp/sepolicy/common/private