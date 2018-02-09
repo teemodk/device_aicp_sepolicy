d602eae99b712dbc5d74119a412abbfe10ffd668
LuK1337 <priv.luk@gmail.com>
2018-02-09 15:43:11 +0100

    sepolicy: common: Lol three spaces
    Change-Id: Ia420ce23968cd1e245ca8272f5377725658f1f5c

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -10,4 +10,4 @@ BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
     device/aicp/sepolicy/common/private
 
 BOARD_SEPOLICY_DIRS += \
-   device/aicp/sepolicy/common/vendor
+    device/aicp/sepolicy/common/vendor
