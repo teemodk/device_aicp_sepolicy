19078f8b12e465183163bc664302f592b38fc456
Adrian DC <radian.dc@gmail.com>
2018-02-26 13:04:19 +0300

    sepolicy: Isolate qcom sysfs_graphics policy for recovery Part II
     * The sysfs_graphics context is not defined globally
        but only by qcom common sepolicies
    
     * Introduced by commit:
        sepolicy: Initial policy for recovery
        Change-Id: Iac57b472dd4628b9bd27fbe47fbbb35c09743be2
        Date: Sat, 17 Feb 2018 01:17:34 +0100
        From: Tom Marshall <tdm.code@gmail.com>
    
    PS2: PS5 from https://review.lineageos.org/#/c/207724/
    
    Change-Id: I35f17a047227f6512275bf55e29a8932d8770bc0

--- qcom/sepolicy.mk
+++ qcom/sepolicy.mk
@@ -3,6 +3,9 @@
 # that inherit from AICP
 #
 
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/aicp/sepolicy/qcom/private
+
 BOARD_SEPOLICY_DIRS += \
     device/aicp/sepolicy/qcom/common \
     device/aicp/sepolicy/qcom/$(TARGET_BOARD_PLATFORM)
