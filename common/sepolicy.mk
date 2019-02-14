414f93756af36f0dcf2cd159aad2495971f2bdbc
dianlujitao <dianlujitao@lineageos.org>
2019-02-14 13:28:42 +0800

    sepolicy: Dynamically build trust policy into system/vendor
     * Introduce a new board flag TARGET_USES_PREBUILT_VENDOR_SEPOLICY and
       two sepolicy variants: dynamic and priv_vendor
     * When TARGET_USES_PREBUILT_VENDOR_SEPOLICY=true, dynamic and
       priv_vendor act as private policy, and vendor policy is excluded in
       order to avoid conflicts (it's not packaged into final builds
       anyway), When the flag is not set, dynamic acts as vendor policy and
       priv_vendor is execluded.
     * Trust policy in priv_vendor is taken from xiaomi sdm845-common.
    
    AICPified and taken out stuff we don't need.
    
    Change-Id: I8bfd078d6064616c88e2c58a9fa3aa045dddf303

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -9,5 +9,12 @@ BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
     device/aicp/sepolicy/common/private
 
+ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/aicp/sepolicy/common/dynamic \
+    device/aicp/sepolicy/common/priv_vendor
+else
 BOARD_SEPOLICY_DIRS += \
+    device/aicp/sepolicy/common/dynamic \
     device/aicp/sepolicy/common/vendor
+endif
--- qcom/sepolicy.mk
+++ qcom/sepolicy.mk
@@ -6,6 +6,8 @@
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
     device/aicp/sepolicy/qcom/private
 
+ifneq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 BOARD_SEPOLICY_DIRS += \
     device/aicp/sepolicy/qcom/common \
     device/aicp/sepolicy/qcom/$(TARGET_BOARD_PLATFORM)
+endif
