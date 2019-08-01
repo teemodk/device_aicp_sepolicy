LineageOS/android_device_lineage_sepolicy

sepolicy: qcom: Rename common to vendor to avoid confusion
 * Remove platform specific rule from sepolicy.mk as well, it's never a
   usecase.

Change-Id: I8bf6af30407d55beb9b2c5a2afaca091564e1605

qcom/sepolicy.mk
@@ -8,6 +8,5 @@ BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
 
 ifneq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 BOARD_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/qcom/common \
-    device/lineage/sepolicy/qcom/$(TARGET_BOARD_PLATFORM)
+    device/lineage/sepolicy/qcom/vendor
 endif

teemodk/device_aicp_sepolicy

 * Remove platform specific rule from sepolicy.mk as well, it's never
   ever a valid usecase.

qcom/sepolicy.mk
@@ -14,6 +14,5 @@ BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
 
 ifneq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 BOARD_SEPOLICY_DIRS += \
-    device/aicp/sepolicy/qcom/common \
-    device/aicp/sepolicy/qcom/$(TARGET_BOARD_PLATFORM)
+    device/aicp/sepolicy/qcom/vendor
 endif